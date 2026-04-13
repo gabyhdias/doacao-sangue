fa7d929db43e4cc3df5f510512a1df39f89f722d
gabriela_h_dias@estudante.sesisenai.org.br <gabriela_h_@estudante.sesisenai.org.br>
2026-04-13 11:01:38 -0300

    add

--- scripts/doação.js
+++ scripts/doação.js
@@ -13,7 +13,7 @@ document.getElementById("formDoacao").addEventListener("submit", function (e) {
     let estado = document.getElementById("estado").value;
     
 
-    if(nome.length < 3 ) return alert("Nome Inválido");
+    if(nome.length < 10 ) return alert("Nome Inválido");
     if(idade.value < 18 ) return alert("Idade Inválida");
     if(telefone.lenght < 8 ) return alert("Telefone Inválido");
     if(motivo.lenght < 10 ) return alert("Caracteres Insuficientes");
